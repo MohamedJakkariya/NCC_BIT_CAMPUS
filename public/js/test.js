8aff161c86829167282dc6fa5e58dac1ae9e064a
mdjacksparrow <mdjack01122000@gmail.com>
2020-03-19 20:14:33 +0530

    add year based search option

---
+++ public/js/test.js
@@ -0,0 +1,19 @@
+
+function byYear() {
+  let findYear = document.querySelector('#getYear');
+  console.log(findYear);
+
+  const allmembers = document.querySelectorAll('.member-year');
+
+  allmembers.forEach(member => {
+      if (findYear.value == member.textContent) {
+          // get parent node
+          member.parentElement.parentElement.parentElement.style.display = 'flex';
+    } else {
+          // get parent node
+      member.parentElement.parentElement.parentElement.style.display = 'none';
+    }
+  });
+
+  console.log(year);
+}
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index');
-});
+});  
 
 // Student signup route
 app.get('/student/signup', (req, res) => {
