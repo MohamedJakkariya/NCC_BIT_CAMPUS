01bd8a5d22ca9f548ad7f94821066811b9c5986e
mdjacksparrow <mdjack01122000@gmail.com>
2020-03-21 16:48:11 +0530

    Add all search based functionality boomm....

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,29 @@
+let findDepartment = document.querySelector('#getDepartment');
+const allMemberOfDepartment = document.querySelectorAll('.member-department');
+
+let findName = document.querySelector('#getName');
+const allMemberOfName = document.querySelectorAll('.member-name');
+
+const findPlace = document.querySelector('#getPlace');
+const allMemberOfPlace = document.querySelectorAll('.member-home-town');
+
+let findYear = document.querySelector('#getYear');
+const allMemberOfYear = document.querySelectorAll('.member-year');
+
 $(document).ready(function() {
-  $('.search-field').hover(
-    function() {
-      $(this)
-        .children('.show')
-        .show();
-    },
-    function() {
-      $(this)
-        .children('.show')
-        .hide();
-    }
-  );
+  // For slide effect 
+  // $('.search-field').hover(
+  //   function() {
+  //     $(this)
+  //       .children('.show')
+  //       .show();
+  //   },
+  //   function() {
+  //     $(this)
+  //       .children('.show')
+  //       .hide();
+  //   }
+  // );
 
   // Slide show
   $('.lazy').slick({
@@ -63,21 +76,176 @@ $(document).ready(function() {
   });
 });
 
-function byYear() {
-  let findYear = document.querySelector('#getYear');
-  console.log(findYear);
+// Place based search 
+findPlace.addEventListener('input', e => {
+  console.log(e.target.value);
 
-  const allmembers = document.querySelectorAll('.member-year');
+  // To parse all members one by one
+  allMemberOfPlace.forEach(member => {
+    console.log(member.textContent[0]);
 
-  allmembers.forEach(member => {
-    if (findYear.value == member.textContent) {
-      // get parent node
-      member.parentElement.parentElement.parentElement.style.display = 'flex';
+    const keyArr = String(e.target.value).split('');
+    console.log(keyArr + ' with length = ' + keyArr.length);
+
+    if (!keyArr.length) {
+      allMemberOfPlace.forEach(member => {
+        member.parentElement.parentElement.style.display = 'flex';
+      });
     } else {
-      // get parent node
-      member.parentElement.parentElement.parentElement.style.display = 'none';
+      // to parst a member into array of all character
+      for (let i = 0; i < keyArr.length; i++) {
+        if (
+          keyArr[i].toLowerCase() == String(member.textContent[i]).toLowerCase()
+        ) {
+          if(
+            keyArr[i - 1] == undefined
+            ){
+              continue;
+            }
+            else if(
+            keyArr[i - 1].toLowerCase() == String(member.textContent[i - 1]).toLowerCase()
+            ){
+              member.parentElement.parentElement.style.display = 'flex';
+            }
+            else{
+              member.parentElement.parentElement.style.display = 'none';
+            }
+        } else {
+          member.parentElement.parentElement.style.display = 'none';
+        }
+      }
     }
   });
+});
 
-  console.log(year);
-}
+
+// Year based search 
+findYear.addEventListener('input', e => {
+  console.log(e.target.value);
+
+  // To parse all members one by one
+  allMemberOfYear.forEach(member => {
+    console.log(member.textContent[0]);
+
+    const keyArr = String(e.target.value).split('');
+    console.log(keyArr + ' with length = ' + keyArr.length);
+
+    if (!keyArr.length) {
+      allMemberOfYear.forEach(member => {
+        member.parentElement.parentElement.parentElement.style.display = 'flex';
+      });
+    } else {
+      // to parst a member into array of all character
+      for (let i = 0; i < keyArr.length; i++) {
+        if (
+          keyArr[i].toLowerCase() === String(member.textContent[i]).toLowerCase()
+        ) {
+          if(
+            keyArr[i - 1] == undefined
+            ){
+              continue;
+            }
+            else if(
+            keyArr[i - 1].toLowerCase() == String(member.textContent[i - 1]).toLowerCase()
+            ){
+              member.parentElement.parentElement.parentElement.style.display = 'flex';
+            }
+            else{
+              member.parentElement.parentElement.parentElement.style.display = 'none';
+            }
+        } else {
+          member.parentElement.parentElement.parentElement.style.display = 'none';
+        }
+      }
+
+    }
+  });
+});
+
+
+// Name based searching 
+findName.addEventListener('input', e => {
+  console.log(e.target.value);
+
+  // To parse all members one by one
+  allMemberOfName.forEach(member => {
+    console.log(member.textContent[0]);
+
+    const keyArr = String(e.target.value).split('');
+    console.log(keyArr + ' with length = ' + keyArr.length);
+
+    if (!keyArr.length) {
+      allMemberOfName.forEach(member => {
+        member.parentElement.parentElement.style.display = 'flex';
+      });
+    } else {
+      // to parst a member into array of all character
+      for (let i = 0; i < keyArr.length; i++) {
+        if (
+          keyArr[i].toLowerCase() === String(member.textContent[i]).toLowerCase()
+        ) {
+          if(
+            keyArr[i - 1] == undefined
+            ){
+              continue;
+            }
+            else if(
+            keyArr[i - 1].toLowerCase() == String(member.textContent[i - 1]).toLowerCase()
+            ){
+              member.parentElement.parentElement.style.display = 'flex';
+            }
+            else{
+              member.parentElement.parentElement.style.display = 'none';
+            }
+        } else {
+          member.parentElement.parentElement.style.display = 'none';
+        }
+      }
+
+    }
+  });
+});
+
+
+// For Department based search 
+findDepartment.addEventListener('input', e => {
+  console.log(e.target.value);
+
+  // To parse all members one by one
+  allMemberOfDepartment.forEach(member => {
+    console.log(member.textContent[0]);
+
+    const keyArr = String(e.target.value).split('');
+    console.log(keyArr + ' with length = ' + keyArr.length);
+
+    if (!keyArr.length) {
+      allMemberOfDepartment.forEach(member => {
+        member.parentElement.parentElement.parentElement.style.display = 'flex';
+      });
+    } else {
+      // to parst a member into array of all character
+      for (let i = 0; i < keyArr.length; i++) {
+        if (
+          keyArr[i].toLowerCase() === String(member.textContent[i]).toLowerCase()
+        ) {
+          if(
+            keyArr[i - 1] == undefined
+            ){
+              continue;
+            }
+            else if(
+            keyArr[i - 1].toLowerCase() == String(member.textContent[i - 1]).toLowerCase()
+            ){
+              member.parentElement.parentElement.parentElement.style.display = 'flex';
+            }
+            else{
+              member.parentElement.parentElement.parentElement.style.display = 'none';
+            }
+        } else {
+          member.parentElement.parentElement.parentElement.style.display = 'none';
+        }
+      }
+
+    }
+  });
+});
--- public/js/test.js
+++ public/js/test.js
@@ -1,19 +1,45 @@
-function byYear() {
-    let findYear = document.querySelector('#getYear');
-    console.log(findYear);
-  
-    const allmembers = document.querySelectorAll('.member-year');
-  
-    allmembers.forEach(member => {
-      if (findYear.value == member.textContent) {
-        // get parent node
-        member.parentElement.parentElement.parentElement.style.display = 'flex';
-      } else {
-        // get parent node
-        member.parentElement.parentElement.parentElement.style.display = 'none';
+const findPlace = document.querySelector('#getPlace');
+const allMemberOfPlace = document.querySelectorAll('.member-home-town');
+
+// Place based search 
+findPlace.addEventListener('input', e => {
+  console.log(e.target.value);
+
+  // To parse all members one by one
+  allMemberOfPlace.forEach(member => {
+    console.log(member.textContent[0]);
+
+    const keyArr = String(e.target.value).split('');
+    console.log(keyArr + ' with length = ' + keyArr.length);
+
+    if (!keyArr.length) {
+      allMemberOfPlace.forEach(member => {
+        member.parentElement.parentElement.style.display = 'flex';
+      });
+    } else {
+      // to parst a member into array of all character
+      for (let i = 0; i < keyArr.length; i++) {
+        // check present character match 
+        if (
+          keyArr[i].toLowerCase() == String(member.textContent[i]).toLowerCase()
+        ) {
+          if(
+          keyArr[i - 1] == undefined
+          ){
+            continue;
+          }
+          else if(
+          keyArr[i - 1].toLowerCase() == String(member.textContent[i - 1]).toLowerCase()
+          ){
+            member.parentElement.parentElement.style.display = 'flex';
+          }
+          else{
+            member.parentElement.parentElement.style.display = 'none';
+          }
+        } else {
+          member.parentElement.parentElement.style.display = 'none';
+        }
       }
-    });
-  
-    console.log(year);
-  }
-  
+    }
+  });
+});
