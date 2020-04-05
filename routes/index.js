aab2c77e9ff64e8d16b3c108a6652e3e901a6b83
mdjacksparrow <mdjack01122000@gmail.com>
2020-04-05 16:03:08 +0530

    successfully authenticate different tables

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated} = require('../config/auth');
+const { ensureAuthenticated } = require('../config/auth');
 
 // Welcome Page
-router.get('/', (req, res) => res.render('index'));
+router.get('/',(req, res) => res.render('index'));
 
 // Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
+router.get('/dashboard',ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
     user: req.user
   })
 );
 
-router.get('/profile', ensureAuthenticated,(req, res) => {
+router.get('/profile',ensureAuthenticated,(req, res) => {
   console.log(req.user);
   
   res.render('profile', {
@@ -25,3 +25,4 @@ router.get('/update', ensureAuthenticated,(req, res) => {
   res.send('Update workspace');
 });
 module.exports = router;
+ 
